c5521876bc2285712b9e158d5a03eeccafa6db54
Tess Hughes <avianthrope101@gmail.com>
2023-02-12 15:56:25 -0500

    Fixed Exit command

--- Commands.py
+++ Commands.py
@@ -62,17 +62,3 @@ def loadPuzzle():
 
 def showStatus():
     print()
-
-def exitCommand(userLetters, requiredLetter, guessedWords, wordBank, totalPoints):
-    print("Whoa, slow down buddy! You're about to lose all of your epic progress, would you like to save your game first? yes/no")
-    userInput = input()
-    while(userInput != "yes") and (userInput != "no"):
-        userInput = input("Please enter \"yes\" or \"no:\": ")
-    if(userInput == "yes"):
-        savePuzzle(userLetters, requiredLetter, guessedWords, wordBank, totalPoints)
-        print("Puzzle saved! Goodbye!")
-    elif(userInput == "no"):
-        #Exit
-        print("Okay! See you on the other side!")
-        exit()
-    sys.exit()
