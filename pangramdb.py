b96d17e8ba812a56725a4e97f6112fa557ef589f
Tess Hughes <avianthrope101@gmail.com>
2023-02-18 18:45:10 -0500

    Added Random Base Word Function using DB

--- pangramdb.py
+++ pangramdb.py
@@ -3,6 +3,7 @@ import json
 con = sqlite3.connect('pangrams.db')
 cursor = con.cursor()
 
+#SQLITE initializes table. do not need to run again since the table is already created !!!
 #cursor.execute('''CREATE TABLE pangrams (pangram TEXT)''')
 
 
@@ -14,13 +15,23 @@ for test in wordlist:
     cursor.execute("INSERT INTO pangrams VALUES (:word)", test)
 '''
 
-
+''' #Some code to print all the pangrams for testing purposes
 cursor.execute("SELECT * FROM pangrams")
 displayWords = cursor.fetchall()
 for test2 in displayWords:
     print(test2)
+'''
+    
+# function to choose a random pangram for use as the base word in a puzzle
+def randomBase():
+    cursor.execute("SELECT * FROM pangrams ORDER BY RANDOM() LIMIT 1;")
+    randomPangram = cursor.fetchone()
+    str = ''
+    for item in randomPangram:
+        str = str + item
+    print(str)
 
 
-
+#SQLITE functions, neccessary
 con.commit() 
 con.close()
