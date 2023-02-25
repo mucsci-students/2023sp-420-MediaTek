730041d2794b99a3edb8b31668ea4d60b3e9f836
SteamsDev <dev208@msn.com>
2023-02-25 16:50:06 -0500

    Update gametest.py

--- gametest.py
+++ gametest.py
@@ -3,15 +3,19 @@ import modelrefactor
 import controllerrefactor
 import wordlist as WL
 import IdentifyBaseWord as base
+import json
 
 class gametest:
     
-    self.testmodel = modelrefactor.model
-    self.testcontroller = controllerrefactor.controller
-    self.testLetters = "whiskey"
-    self.testReq = "e"
+    # Constructor
+    def __init__(self):
+        self.testmodel = modelrefactor.model
+        self.testcontroller = controllerrefactor.controller
+        self.testLetters = "whiskey"
+        self.testReq = "e"
+        
     
-    #Test get list
+    # Test get list
     def testGetList(self):
         self.testmodel.p1.gaUserLetters = self.testLetters
         self.testmodel.p1.gaReqLetter = self.testReq
@@ -30,5 +34,30 @@ class gametest:
         assert self.testLetters == baseLetters
         assert self.testReq == baseReq
     
+    # Test base game
+    def testBaseGame(self):
+        pangram = "mediocre"
+        self.testLetters, self.ReqLetter = self.testmodel.np.baseGame()
+        assert not len(self.testLetters) == 0
+        assert not len(self.testReq) == 0
+        for i in self.testLetters:
+            assert not self.testLetters.find(1) == -1
     
-        
+    # Test save game
+    def testSaveGame(self):
+        self.testLetters = "whiskey"
+        self.testReq = "e"
+        gameName = "testName"
+        self.testmodel.saveGame(gameName)
+        assert not len(json.loads(gameName)) == 0
+    
+    # Test load game
+    def testLoadGame(self):
+        self.testLetters = "whiskey"
+        self.testReq = "e"
+        gameName = "testName"
+        self.testmodel.gameLoad(gameName)
+        assert not len(self.testmodel.p1.gaUseretters) == 0
+        assert not len(self.testmodel.p1.gaReqetter) == 0
+        assert self.testmodel.p1.gaUserLetters == self.testLetters
+        assert self.testmodel.p1.gaReqLetter == self.testReq
