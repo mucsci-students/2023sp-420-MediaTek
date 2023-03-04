46e606a2b34a3cf731b3cfbf3560ef9778398271
SteamsDev <dev208@msn.com>
2023-03-03 20:17:32 -0500

    Create unittest.py

---
+++ unittest.py
@@ -0,0 +1,44 @@
+import unittest
+from unittest import mock
+import savegame
+import loadgame
+import modelrefactor
+import IdentifyBaseWord as base
+
+
+class unittest:
+    
+    def __init__(self):
+        self.player = modelrefactor.player()
+        self.model = modelrefactor.model()
+        self.base = base
+        
+    def input_function(self, default_response=None):
+        
+        def _input(prompt):
+            return default_response if default_response else input(prompt)
+
+        return _input
+    
+    def testInvalidPangram(self):
+        self.pangram = "pangram"
+        with mock.patch("base.input", new=input_function(self.pangram)):
+            self.base.baseGame()
+            assert self.model.getGameState() == 0
+            assert len(self.model.getLetter()) == 0
+        
+        self.pangram = "psychiatrist"
+        with mock.patch("base.input", new=input_function(self.pangram)):
+            self.base.baseGame()
+            assert self.model.getGameState() == 0
+            assert len(self.model.getLetter()) == 0
+    
+    def testPangramSevenLetters(self):
+        self.pangram = "honesty"
+        with mock.patch("base.input", new=input_function(self.pangram)):
+            self.base.baseGame()
+            assert self.model.getGameState() == 1
+            assert len(self.model.getLetter()) == 7
+            
+    def 
+
