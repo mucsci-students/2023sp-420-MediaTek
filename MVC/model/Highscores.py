b93b4aab3c796ec84a0a9cc4e6bd1984b50fe91b
Gabriel Zimmermann <80365452+gabe2762@users.noreply.github.com>
2023-04-15 13:01:21 -0400

    Highscore.py comment update
    Consistent commenting structure

--- MVC/model/Highscores.py
+++ MVC/model/Highscores.py
@@ -6,7 +6,9 @@ check_dir3 = os.path.dirname(os.path.abspath(__file__))
 db_dir3 = os.path.join(check_dir3,".","highscores.db")
 abs_path3 = os.path.abspath(db_dir3)
 
-#creates the highscores table in the highscore database
+'''
+creates the highscores table in the highscore database
+'''
 def create_highscores_table():
     con = cdb.highscoreConnect()
     c = con.cursor()
@@ -19,11 +21,13 @@ def create_highscores_table():
 
     con.commit()
     con.close()
-    
-#saves highscore for a specific game_id along with player name and points scored to the aforementioned database.
-#game_id (str): The unique identifier for the game.
-#player_name (str): The player's name.
-#points (int): The player's points.
+
+'''
+saves highscore for a specific game_id along with player name and points scored to the aforementioned database.
+game_id (str): The unique identifier for the game.
+player_name (str): The player's name.
+points (int): The player's points.
+'''
 def saveHighScore(game_id, player_name, points):
     con = cdb.highscoreConnect()
     cursor = con.cursor()
@@ -32,24 +36,28 @@ def saveHighScore(game_id, player_name, points):
     con.commit()
     con.close()
 
-#Loads the top ten local highscores for a specific game_id
-#game_id (str): The unique identifier for the game.
-#limit (int, optional): The number of high scores to retrieve. Defaults to 10.
-#returns:
-#list[dict]: A list of dictionaries with the player's name and points.
+'''
+Loads the top ten local highscores for a specific game_id
+game_id (str): The unique identifier for the game.
+limit (int, optional): The number of high scores to retrieve. Defaults to 10.
+returns:
+list[dict]: A list of dictionaries with the player's name and points.
+'''
 def loadHighScore(game_id):
     con = cdb.highscoreConnect()
     cursor = con.cursor()
 
-    # Retrieve the top 10 high scores for the given game_id
+    #Retrieve the top 10 high scores for the given game_id
     cursor.execute("SELECT game_id, player_name, points FROM highscores WHERE game_id = ? ORDER BY points DESC LIMIT 10", (game_id,))
     rows = cursor.fetchall()
 
     con.close()
     return rows
 
-#Displays the contents of highscore table for testing purposes
-#need to make sure the database is updating correctly
+'''
+Displays the contents of highscore table for testing purposes
+need to make sure the database is updating correctly
+'''
 def display_highscores():
     con = cdb.highscoreConnect()
     cursor = con.cursor()
@@ -62,7 +70,9 @@ def display_highscores():
         print(f"Game ID: {row[0]}, Player Name: {row[1]}, Points: {row[2]}")
 
     con.close()
-    
-    
-# Call create_highscores_table to ensure the table exists before any operations
+
+
+'''
+Call create_highscores_table to ensure the table exists before any operations
+'''
 create_highscores_table()
